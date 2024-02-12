e4bc0bda9c8216935e25f8c2d0046a62d07f692c
Thiago Soares <thg.ol.so@gmail.com>
2024-02-12 16:16:33 -0400

    refactor: envs

--- src/app/config/envs.py
+++ src/app/config/envs.py
@@ -30,25 +30,26 @@ conf.config["id_farm"] = values.get("FARM")
 URL_DB = conf.config.get("url_db")
 DATABASE = conf.config.get("db_name")
 
-TOKEN = conf.config.get("token")
-URL_FARM = conf.config.get("url_farm")
-ID_FARM = int(conf.config.get("id_farm"))
-PORT_FARM = int(conf.config.get("port", 52000)) + ID_FARM
 
+if conf.config.get("id_farm"):
+    TOKEN = conf.config.get("token")
+    URL_FARM = conf.config.get("url_farm")
+    ID_FARM = int(conf.config.get("id_farm"))
+    PORT_FARM = int(conf.config.get("port", 52000)) + ID_FARM
 
-FIRST_SYNC = conf.config.get("first_sync")
-IS_SYNC = conf.config.get("is_to_sync", False)
+    FIRST_SYNC = conf.config.get("first_sync")
+    IS_SYNC = conf.config.get("is_to_sync", False)
+
+    if not (URL_DB and DATABASE and TOKEN and URL_FARM and ID_FARM):
+        print(
+            f"""\
+            URL_DB: {URL_DB is not None},
+            DATABASE: {DATABASE is not None},
+            TOKEN: {TOKEN is not None},
+            URL_FARM: {URL_FARM is not None},
+            ID_FARM: {ID_FARM is not None}\
+            """
+        )
+
 
 MAX_RECORD_SYNC = conf.config.get("max_record_sync", 1000)
-
-if not (URL_DB and DATABASE and TOKEN and URL_FARM and ID_FARM):
-    print(
-        f"""\
-        URL_DB: {URL_DB is not None},
-        DATABASE: {DATABASE is not None},
-        TOKEN: {TOKEN is not None},
-        URL_FARM: {URL_FARM is not None},
-        ID_FARM: {ID_FARM is not None}\
-        """
-    )
-    exit(0)
